b2bc8fef859d519c5d4e7d8d89c2bf87260a2ca5
piera <l.piera@protonmail.com>
2023-05-09 11:18:51 +0200

    feat: add fetching data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import {GraphQLClient} from "graphql-request";
+import {SUBGRAPH_URL} from "@/consts";
+import {VaultsDocument} from "@/queries/core";
+
 export const runtime = "edge";
 
-export default function RootPage() {
+async function getData() {
+  const client = new GraphQLClient(SUBGRAPH_URL, { fetch: fetch })
+  return await client.request(VaultsDocument)
+}
+
+export default async function RootPage() {
+
+  const data = await getData();
+  console.log(data)
+
   return <main className="flex min-h-screen flex-col items-center justify-between p-24">Hello world!</main>;
 }
