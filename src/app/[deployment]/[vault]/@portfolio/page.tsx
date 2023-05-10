8c13fa4b34456d1e474944330a7a49f6ffc59d67
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-05-10 16:38:31 +0200

    feat: update portfolio page

--- src/app/[deployment]/[vault]/@portfolio/page.tsx
+++ src/app/[deployment]/[vault]/@portfolio/page.tsx
@@ -1,4 +1,11 @@
 import { TokenHoldingsTable } from "@/components/TokenHoldingsTable";
+import { getNetworkByDeployment } from "@/lib/consts";
+import { handleContractError } from "@/lib/errors";
+import { assertParams } from "@/lib/params";
+import { getAssetInfo } from "@/lib/rpc/getAssetInfo";
+import { getBalance } from "@/lib/rpc/getBalance";
+import { getTrackedAssets } from "@/lib/rpc/getTrackedAssets";
+import { z } from "@/lib/zod";
 
 const mockPortfolioData = [
   {
@@ -15,8 +22,30 @@ const mockPortfolioData = [
   },
 ];
 
-export default function PortfolioPage() {
-  return <>{TokenHoldingsTable(mockPortfolioData)}</>;
+export default async function PortfolioPage({ params }: { params: { deployment: string; vault: string } }) {
+  const { vault, deployment } = assertParams({
+    params,
+    schema: z.object({
+      deployment: z.deployment(),
+      vault: z.address(),
+    }),
+  });
+  
+  const network = getNetworkByDeployment(deployment);
+  
+  const trackedAssets = await getTrackedAssets({ vault, network }).catch(handleContractError());
+
+  const portfolioAssets = await Promise.all(
+    trackedAssets.map(async (asset) => (
+      { asset: await getAssetInfo({ network, asset }), balance: await getBalance({ network, account: vault, asset })}
+    ))
+  ).catch(handleContractError());
+
+  return (
+    <>
+      {TokenHoldingsTable(portfolioAssets)}
+    </>
+  );
 }
 
 export const runtime = "edge";
--- src/components/TokenHoldingsTable.tsx
+++ src/components/TokenHoldingsTable.tsx
@@ -1,28 +1,37 @@
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { type Address, formatUnits } from "viem";
 
-export function TokenHoldingsTable(portfolio: any) {
+interface PortfolioAssets {
+  asset: {
+    name: string,
+    symbol: string,
+    decimals: bigint,
+    address: Address
+  },
+  balance: bigint,
+}
+
+export function TokenHoldingsTable(portfolioAssets: PortfolioAssets[]) {
   return (
     <div className="w-full">
       <h1 className="my-4 text-lg">Token Holdings</h1>
-      {portfolio.length > 0 ? (
-        <Table>
-          <TableCaption>A list of your current Portfolio assets!</TableCaption>
-          <TableHeader className="w-full">
-            <TableRow>
-              <TableHead className="w-[100px]">Asset</TableHead>
-              <TableHead>Balance</TableHead>
-              <TableHead>Price</TableHead>
-              <TableHead className="text-right">Value</TableHead>
+      {portfolioAssets.length > 0 ? (
+      <Table>
+        <TableCaption>A list of your current Portfolio assets!</TableCaption>
+        <TableHeader className="w-full">
+          <TableRow>
+            <TableHead>Asset</TableHead>
+            <TableHead className="text-right">Balance</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {portfolioAssets.map((row: any) => (
+            <TableRow key={row.asset.symbol}>
+              <TableCell className="font-medium">
+                {row.asset.symbol} {row.asset.name}
+              </TableCell>
+              <TableCell className="text-right">{formatUnits(row.balance, Number(row.asset.decimals))}</TableCell>
             </TableRow>
-          </TableHeader>
-          <TableBody>
-            {portfolio.map((row: any) => (
-              <TableRow>
-                <TableCell className="font-medium">{row.asset}</TableCell>
-                <TableCell>{row.balance}</TableCell>
-                <TableCell>{row.price}</TableCell>
-                <TableCell className="text-right">{row.value}</TableCell>
-              </TableRow>
             ))}
           </TableBody>
         </Table>
