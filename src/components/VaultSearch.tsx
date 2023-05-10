178668aadcb2581f3d412f48b883d706e809e138
Sebastian Lorenz <fubhy@fubhy.com>
2023-05-10 16:12:20 +0200

    refactor: previous data tweeks

--- src/components/VaultSearch.tsx
+++ src/components/VaultSearch.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "./ui/command";
-import { type Deployment, getNetworkByDeployment } from "@/lib/consts";
+import { type Deployment, deployments, getNetworkByDeployment } from "@/lib/consts";
 import { VaultBasicInfoFragmentDoc, queryCoreSubgraph, useFragment } from "@/lib/gql";
 import { getAssetSymbol } from "@/lib/rpc/getAssetSymbol";
 import { getVaultName } from "@/lib/rpc/getVaultName";
 import { vaultSearch } from "@/lib/subgraphs/core/vaultSearch";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useDebounce } from "react-use";
 import { type Address, isAddress } from "viem";
 
@@ -46,54 +46,49 @@ function useVaultSearch(deployment: Deployment, query: string) {
         return { name: fragment.name, symbol: fragment.symbol, address: fragment.id as Address };
       });
     },
-    keepPreviousData: true,
+    keepPreviousData: significant && !isAddress(query),
   });
 
-  return significant ? result : undefined;
+  return result;
 }
 
 export function VaultSearch() {
-  const router = useRouter();
   const [value, setValue] = useState("");
   const [debounced, setDebounced] = useState("");
   useDebounce(() => setDebounced(value), 250, [value]);
-
-  const ethereum = useVaultSearch("ethereum", debounced);
-  const polygon = useVaultSearch("polygon", debounced);
-
-  const ethereumData = ethereum?.data ?? [];
-  const polygonData = polygon?.data ?? [];
 
   return (
     <Command shouldFilter={false} className="rounded-lg border shadow-md">
       <CommandInput placeholder="Search vaults ..." onValueChange={(value) => setValue(value)} />
       <CommandEmpty>No vaults found.</CommandEmpty>
-      {(ethereumData.length ?? 0) > 0 ? (
-        <CommandGroup heading="Ethereum">
-          {ethereumData.map((item) => (
-            <CommandItem
-              key={item.address}
-              value={item.address}
-              onSelect={() => router.push(`/ethereum/${item.address}`)}
-            >
-              {item.name}
-            </CommandItem>
-          ))}
-        </CommandGroup>
-      ) : null}
-      {(polygonData.length ?? 0) > 0 ? (
-        <CommandGroup heading="Polygon">
-          {polygonData.map((item) => (
-            <CommandItem
-              key={item.address}
-              value={item.address}
-              onSelect={() => router.push(`/polygon/${item.address}`)}
-            >
-              {item.name}
-            </CommandItem>
-          ))}
-        </CommandGroup>
-      ) : null}
+      {deployments.map((deployment) => (
+        <VaultSearchDeploymentGroup key={deployment} deployment={deployment} query={debounced} />
+      ))}
     </Command>
+  );
+}
+
+function VaultSearchDeploymentGroup({ deployment, query }: { deployment: Deployment; query: string }) {
+  const router = useRouter();
+  const heading = useMemo(() => `${deployment.slice(0, 1).toUpperCase()}${deployment.slice(1)}`, [deployment]);
+  const result = useVaultSearch(deployment, query);
+  const data = result.data ?? [];
+
+  if (data.length === 0) {
+    return null;
+  }
+
+  return (
+    <CommandGroup heading={heading}>
+      {data.map((item) => (
+        <CommandItem
+          key={item.address}
+          value={item.address}
+          onSelect={() => router.push(`/${deployment}/${item.address}`)}
+        >
+          {item.name}
+        </CommandItem>
+      ))}
+    </CommandGroup>
   );
 }
