7a72a822930046a09bf5cb43e02cd4082b787c12
Ray Phelan <rayphelan@gmail.com>
2023-05-11 16:03:21 +0100

    chore: update policy rpc

--- src/app/[deployment]/[vault]/(tabs)/@configuration/page.tsx
+++ src/app/[deployment]/[vault]/(tabs)/@configuration/page.tsx
@@ -4,27 +4,11 @@ import {
   MIN_MAX_INVESTMENT_POLICY,
   getNetworkByDeployment,
 } from "@/lib/consts";
-import {
-  ENTRANCE_RATE_BURN_FEE,
-  ENTRANCE_RATE_DIRECT_FEE,
-  EXIT_RATE_BURN_FEE,
-  EXIT_RATE_DIRECT_FEE,
-  MANAGEMENT_FEE,
-  PERFORMANCE_FEE,
-} from "@/lib/consts";
 import { assertParams } from "@/lib/params";
 import { getAllowedDepositRecipientsPolicy } from "@/lib/rpc/getAllowedDepositRecipientsPolicy";
 import { getAllowedSharesTransferRecipientsPolicy } from "@/lib/rpc/getAllowedSharesTransferRecipientsPolicy";
-import { getEnabledFeesForFund } from "@/lib/rpc/getEnabledFeesForFund";
 import { getEnabledPoliciesForFund } from "@/lib/rpc/getEnabledPoliciesForFund";
-import { getEntranceRateBurnFee } from "@/lib/rpc/getEntranceRateBurnFee";
-import { getEntranceRateDirectFee } from "@/lib/rpc/getEntranceRateDirectFee";
-import { getExitRateBurnFee } from "@/lib/rpc/getExitRateBurnFee";
-import { getExitRateDirectFee } from "@/lib/rpc/getExitRateDirectFee";
-import { getFeeManager } from "@/lib/rpc/getFeeManager";
-import { getManagementFee } from "@/lib/rpc/getManagementFee";
 import { getMinMaxInvestmentPolicy } from "@/lib/rpc/getMinMaxInvestmentPolicy";
-import { getPerformanceFee } from "@/lib/rpc/getPerformanceFee";
 import { getPolicyManager } from "@/lib/rpc/getPolicyManager";
 import { getVaultComptroller } from "@/lib/rpc/getVaultComptroller";
 import { z } from "@/lib/zod";
@@ -48,16 +32,6 @@ export default async function ConfigurationPage({
   const network = getNetworkByDeployment(deployment);
 
   const comptrollerProxy = await getVaultComptroller({ network, vault });
-
-  const feeManager = await getFeeManager({ network, comptrollerProxy });
-
-  const enabledFeesForFund = await getEnabledFeesForFund({
-    network,
-    comptrollerProxy,
-    feeManager,
-  });
-
-  console.log("FEES", enabledFeesForFund);
 
   const policyManager = await getPolicyManager({ network, comptrollerProxy });
 
@@ -95,60 +69,6 @@ export default async function ConfigurationPage({
     });
     console.log("ALLOWED_SHARES_TRANSFER_RECIPIENTS_POLICY", result);
   }
-
-  // if (enabledFeesForFund.includes(EXIT_RATE_BURN_FEE)) {
-  //   result = await getExitRateBurnFee({
-  //     network,
-  //     comptrollerProxy,
-  //     address: EXIT_RATE_BURN_FEE,
-  //   });
-  //   console.log("EXIT_RATE_BURN_FEE", result);
-  // }
-
-  // if (enabledFeesForFund.includes(EXIT_RATE_DIRECT_FEE)) {
-  //   result = await getExitRateDirectFee({
-  //     network,
-  //     comptrollerProxy,
-  //     address: EXIT_RATE_DIRECT_FEE,
-  //   });
-  //   console.log("EXIT_RATE_DIRECT_FEE", result);
-  // }
-
-  // if (enabledFeesForFund.includes(ENTRANCE_RATE_DIRECT_FEE)) {
-  //   result = await getEntranceRateDirectFee({
-  //     network,
-  //     comptrollerProxy,
-  //     address: ENTRANCE_RATE_DIRECT_FEE,
-  //   });
-  //   console.log("ENTRANCE_RATE_DIRECT_FEE", result);
-  // }
-
-  // if (enabledFeesForFund.includes(ENTRANCE_RATE_BURN_FEE)) {
-  //   result = await getEntranceRateBurnFee({
-  //     network,
-  //     comptrollerProxy,
-  //     address: ENTRANCE_RATE_BURN_FEE,
-  //   });
-  //   console.log("ENTRANCE_RATE_BURN_FEE", result);
-  // }
-
-  // if (enabledFeesForFund.includes(PERFORMANCE_FEE)) {
-  //   result = await getPerformanceFee({
-  //     network,
-  //     comptrollerProxy,
-  //     address: PERFORMANCE_FEE,
-  //   });
-  //   console.log("PERF", result);
-  // }
-
-  // if (enabledFeesForFund.includes(MANAGEMENT_FEE)) {
-  //   result = await getManagementFee({
-  //     network,
-  //     comptrollerProxy,
-  //     address: MANAGEMENT_FEE,
-  //   });
-  //   console.log("MANG", result);
-  // }
 
   return (
     <>
