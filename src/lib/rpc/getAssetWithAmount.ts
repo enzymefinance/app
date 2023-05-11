948553177e1e705bdb94db969cf7ac64c65e4fdb
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-05-11 11:18:59 +0200

    feat: add get asset with amount

---
+++ src/lib/rpc/getAssetWithAmount.ts
@@ -0,0 +1,21 @@
+import type { Address } from "viem";
+import type { Network } from "../consts";
+import { getAssetInfo } from "./getAssetInfo";
+import { getAmount } from "./getAmount";
+
+export async function getAssetWithAmount({
+  network,
+  account,
+  asset,
+}: {
+  network: Network;
+  account: Address;
+  asset: Address;
+}) {
+  const [info, amount] = await Promise.all([
+    getAssetInfo({ network, asset }),
+    getAmount({ network, account, asset }),
+  ]);
+
+  return { ...info, amount };
+}
