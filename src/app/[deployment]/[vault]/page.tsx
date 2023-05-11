a95f5701510022345ca430dd57638f0eb7e8ff58
piera <l.piera@protonmail.com>
2023-05-11 12:21:47 +0200

    feat: change routes directory

--- src/app/[deployment]/[vault]/page.tsx
+++ src/app/[deployment]/[vault]/page.tsx
@@ -20,33 +20,9 @@ export default async function VaultPage({ params }: { params: { deployment: stri
     }),
   });
 
-  const network = getNetworkByDeployment(deployment);
-  const [owner, comptroller, trackedAssets, externalPositionsInfo] = await Promise.all([
-    getVaultOwner({ vault, network }),
-    getVaultComptroller({ vault, network }),
-    getTrackedAssets({ vault, network }),
-    getExternalPositionsInfo({ vault, network }),
-  ]).catch(handleContractError());
-
-  const [trackedAssetsInfo, , denominationAsset] = await Promise.all([
-    getAssetInfoMultiple({ network, assets: trackedAssets }),
-    getBalanceMultiple({ network, account: vault, assets: trackedAssets }),
-    getDenominationAsset({ network, comptroller }),
-  ]).catch(handleContractError());
-
-  const denominationAssetInfo = await getAssetInfo({
-    network,
-    asset: denominationAsset,
-  }).catch(handleContractError());
-
   return (
     <div className="grid grid-cols-1 grid-rows-6 gap-4 pt-2 sm:grid-cols-3 sm:grid-rows-2">
-      <VaultTile title="Owner" description={owner} />
-      <VaultTile title="Denomination Asset" description={denominationAssetInfo.symbol} />
-      <VaultTile title="Total supply" description="$0.99" />
-      <VaultTile title="GAV" description="$1,001.52" />
-      <VaultTile title="Share price" description="$1,001.52" />
-      <VaultTile title="Release" description="Sulu" />
+      <VaultTile title="Vault" description={vault} />
     </div>
   );
 }
