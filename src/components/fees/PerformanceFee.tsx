bb033fa9e2df5961cadd9d87541efd7909b85cd1
piera <l.piera@protonmail.com>
2023-05-16 12:13:48 +0200

    feat: rewrite fees

--- src/components/fees/PerformanceFee.tsx
+++ src/components/fees/PerformanceFee.tsx
@@ -1,12 +1,10 @@
 import { asSyncComponent } from "@/lib/next";
 import { type Deployment, ZERO_ADDRESS } from "@/lib/consts";
-import { type Address, parseAbi } from "viem";
+import { type Address } from "viem";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { BigIntDisplay } from "@/components/BigIntDisplay";
 import { getPublicClientForDeployment } from "@/lib/rpc";
 import { getPerformanceFee, getDenominationAsset, getAssetSymbol, getAssetDecimals } from "@enzymefinance/sdk";
-import { readContract } from "viem/contract";
-import { IComptroller } from "@enzymefinance/abis/IComptroller";
 
 export const PerformanceFee = asSyncComponent(
   async ({
