9a5dad7a477a140500caac8d390249202da3f2fd
Ray Phelan <rayphelan@gmail.com>
2023-05-17 07:51:53 +0100

    refactor: update policies (#12)

--- src/components/policies/AllowedExternalPositionTypesPerManagerPolicy.tsx
+++ src/components/policies/AllowedExternalPositionTypesPerManagerPolicy.tsx
@@ -1,36 +1,13 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import type { Deployment } from "@/lib/consts";
 import { asSyncComponent } from "@/lib/next";
-import { getPublicClientForDeployment } from "@/lib/rpc";
-import { getAllowedExternalPositionTypesPerManagerPolicy } from "@/lib/rpc/getAllowedExternalPositionTypesPerManager";
-import type { Address } from "viem";
 
-export const AllowedExternalPositionTypesPerManagerPolicy = asSyncComponent(
-  async ({
-    deployment,
-    comptrollerProxy,
-    policy,
-    user,
-  }: {
-    deployment: Deployment;
-    comptrollerProxy: Address;
-    policy: Address;
-    user: Address;
-  }) => {
-    const client = getPublicClientForDeployment(deployment);
-    const result = await getAllowedExternalPositionTypesPerManagerPolicy(client, {
-      comptrollerProxy,
-      address: policy,
-      user,
-    });
-
-    return (
-      <Card>
-        <CardHeader>
-          <CardTitle>AllowedExternalPositionTypesPerManagerPolicy</CardTitle>
-        </CardHeader>
-        <CardContent>...</CardContent>
-      </Card>
-    );
-  },
-);
+export const AllowedExternalPositionTypesPerManagerPolicy = asSyncComponent(async () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Allowed External Position Types Per Manager Policy</CardTitle>
+      </CardHeader>
+      <CardContent>...</CardContent>
+    </Card>
+  );
+});
